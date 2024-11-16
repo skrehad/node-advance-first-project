a9c719c2117747f539f21684045d3f427a13d6c0
rehadhasan <rehadhasan664@gmail.com>
2024-11-17 01:20:33 +0600

    install some package

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`App is listening on port ${config.port}`);
+      console.log(`App is listening on Port ${config.port}`);
     });
   } catch (err) {
-    console.error("Failed to connect to the database:", err);
+    console.error('Failed to connect to the database:', err);
   }
 }
 
