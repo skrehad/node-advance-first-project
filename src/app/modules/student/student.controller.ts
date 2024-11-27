2db527e5a2e1007cd1f4ede24debdf0bb7b8e618
rehadhasan <rehadhasan664@gmail.com>
2024-11-28 05:35:11 +0600

    modify student controller

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,33 +1,6 @@
 import { StudentServices } from './student.service';
 import { Request, Response } from 'express';
 import { z } from 'zod';
-import studentValidationSchema from './student.validation';
-
-const createStudent = async (req: Request, res: Response) => {
-  try {
-    const studentData = req.body;
-
-    // Validate the data
-    const zodParseData = studentValidationSchema.parse(studentData);
-
-    // Save the data to the database
-    const result = await StudentServices.createStudentIntoDB(zodParseData);
-
-    // Respond with success
-    res.status(200).json({
-      success: true,
-      message: 'Student created successfully',
-      data: result,
-    });
-  } catch (error: any) {
-    // Handle validation or other errors
-    res.status(400).json({
-      success: false,
-      message: error.message || 'Validation Failed or Something went wrong',
-      error: error instanceof z.ZodError ? error.errors : error,
-    });
-  }
-};
 
 const getAllStudents = async (req: Request, res: Response) => {
   try {
@@ -82,7 +55,6 @@ const getSingleStudent = async (req: Request, res: Response) => {
 // };
 
 export const StudentController = {
-  createStudent,
   getAllStudents,
   getSingleStudent,
   // deleteStudent,
