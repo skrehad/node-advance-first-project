98031ee27a014b6945ddd7d08e024e08a5349a3f
rehadhasan <rehadhasan664@gmail.com>
2024-11-28 05:28:09 +0600

    update user model

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,10 +1,14 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string(),
-  password: z.string().max(15, {
-    message: 'password can not be more than 15 characters',
-  }),
+  password: z
+    .string({
+      invalid_type_error: 'Password must be string',
+    })
+    .max(15, {
+      message: 'password can not be more than 15 characters',
+    })
+    .optional(),
   needsPasswordChange: z.boolean().optional().default(true),
   role: z.enum(['admin', 'student', 'faculty']),
   status: z.enum(['in-progress', 'blocked']).default('in-progress'),
