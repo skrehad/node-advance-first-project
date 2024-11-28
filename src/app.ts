33accf511fde159353de740e640243ec600cad01
rehadhasan <rehadhasan664@gmail.com>
2024-11-29 02:37:42 +0600

    handle routes and app file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrowhandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 // const port = 3000;
 
@@ -12,10 +11,14 @@ app.use(express.json());
 app.use(cors());
 
 // application route
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-app.get('/', (req: Request, res: Response) => {});
+const test = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', test);
 
 app.use(globalErrorHandler);
 app.use(notFound);
