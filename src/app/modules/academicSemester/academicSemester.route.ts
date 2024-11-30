60d7116dd67605fe3e1b0fe96bcc0e28c36737ce
rehadhasan <rehadhasan664@gmail.com>
2024-12-01 02:27:48 +0600

    create a Academic Semester Validations for a middleware

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterControllers } from './academicSemester.controller';
-// import { AcademicSemesterValidations } from './academicSemester.validation';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidations } from './academicSemester.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
-  //   validateRequest(
-  //     AcademicSemesterValidations.createAcdemicSemesterValidationSchema,
-  //   ),
+  validateRequest(
+    AcademicSemesterValidations.createAcademicSemesterValidationSchema,
+  ),
   AcademicSemesterControllers.createAcademicSemester,
 );
 
