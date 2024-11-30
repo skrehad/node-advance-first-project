c636d1777b7f29c5691b6853756e713802f29201
rehadhasan <rehadhasan664@gmail.com>
2024-12-01 03:15:59 +0600

    update interface and constant

--- src/app/modules/academicSemester/academicSemester.constant.ts
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -1,7 +1,7 @@
 import {
   TAcademicSemesterCode,
   TAcademicSemesterName,
-  // TAcademicSemesterNameCodeMapper,
+  TAcademicSemesterNameCodeMapper,
   TMonths,
 } from './academicSemester.interface';
 
@@ -28,8 +28,8 @@ export const AcademicSemesterName: TAcademicSemesterName[] = [
 
 export const AcademicSemesterCode: TAcademicSemesterCode[] = ['01', '02', '03'];
 
-// export const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
-//   Autumn: '01',
-//   Summer: '02',
-//   Fall: '03',
-// };
+export const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+};
