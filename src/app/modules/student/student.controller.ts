60431951966b4db6ab1b75eb0b70f031e1345c59
rehadhasan <rehadhasan664@gmail.com>
2024-11-30 19:24:13 +0600

    create a catchAsync file

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,6 +3,7 @@ import sendResponse from '../../utils/sendResponse';
 import { StudentServices } from './student.service';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
+// for try catch handle by one time calling for all function
 const catchAsync = (fn: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((error) => next(error));
