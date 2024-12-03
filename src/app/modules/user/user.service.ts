05d8a831b6985ad565c3703dbf3d6848e4e2d787
rehadhasan <rehadhasan664@gmail.com>
2024-12-03 23:42:15 +0600

    finis the transaction

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,6 +7,8 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.ultis';
+import AppError from '../../errors/AppError';
+import { HttpStatus } from 'http-status-ts';
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   //   create a user object
@@ -29,21 +31,32 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   const session = await mongoose.startSession();
 
   try {
+    session.startTransaction();
+
     //   manually create id
     userData.id = await generateStudentId(admissionSemester);
 
     // create a user (transaction)
-    const newUser = await User.create(userData);
+    const newUser = await User.create([userData], { session });
 
     //   create a student
-    if (Object.keys(newUser).length) {
-      // set id, _id as User
-      studentData.id = newUser.id;
-      studentData.user = newUser._id; // reference id
-
-      const newStudent = await Student.create(studentData);
-      return newStudent;
+    if (!newUser.length) {
+      throw new AppError(HttpStatus.BAD_REQUEST, 'Failed to create User');
     }
+    // set id, _id as User
+    studentData.id = newUser[0].id;
+    studentData.user = newUser[0]._id; // reference id
+
+    const newStudent = await Student.create([studentData], { session });
+    // create a user (transaction2)
+    if (!newStudent.length) {
+      throw new AppError(HttpStatus.BAD_REQUEST, 'Failed to create Student');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return newStudent;
   } catch (error) {}
 };
 
