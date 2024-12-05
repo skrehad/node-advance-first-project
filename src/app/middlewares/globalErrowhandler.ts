4c55ff760a00791748ac1fe961ff49e0e18c5651
rehadhasan <rehadhasan664@gmail.com>
2024-12-06 02:54:36 +0600

    export Validation Error

--- src/app/middlewares/globalErrowhandler.ts
+++ src/app/middlewares/globalErrowhandler.ts
@@ -1,6 +1,10 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { TErrorSources } from '../interface/error';
 import config from '../config';
+import { ZodError } from 'zod';
+import AppError from '../errors/AppError';
+import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   // setting default value
@@ -12,6 +16,45 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
       message: 'Something went wrong',
     },
   ];
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error?.name === 'CastError') {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error?.code === 11000) {
+    const simplifiedError = handleDuplicateError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error instanceof AppError) {
+    statusCode = error?.statusCode;
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message: error?.message,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message: error?.message,
+      },
+    ];
+  }
 
   //ultimate return
   res.status(statusCode).json({
