a45aea633eaf5a430e23242d64f97a7dbb028579
rehadhasan <rehadhasan664@gmail.com>
2024-12-06 03:10:43 +0600

    comment some code for nex time uderstanding

--- src/app/interface/error.ts
+++ src/app/interface/error.ts
@@ -3,6 +3,7 @@ export type TErrorSources = {
   message: string;
 }[];
 
+// aita hoise kew jeno vul error na pathaite pare mane onno developer jodi ai site niye arekta githup repote kaj kore ar vul val error patay taile jeno na ney
 export type TGenericErrorResponse = {
   statusCode: number;
   message: string;
