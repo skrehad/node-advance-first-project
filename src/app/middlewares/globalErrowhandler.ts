05e5881f0658fe011d9bbec21eddc2bd8036e440
rehadhasan <rehadhasan664@gmail.com>
2024-12-06 01:07:21 +0600

    export TErrorSources

--- src/app/middlewares/globalErrowhandler.ts
+++ src/app/middlewares/globalErrowhandler.ts
@@ -1,8 +1,17 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { TErrorSources } from '../interface/error';
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  // setting default value
   const statusCode = 500;
   const message = 'Something went wrong';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
   res.status(statusCode).json({
     success: false,
     message,
