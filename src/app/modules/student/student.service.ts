9814e5afee795aeeb501b77026220519c0bd0b0f
rehadhasan <rehadhasan664@gmail.com>
2024-12-07 02:58:06 +0600

    QueryBuilder working

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -4,16 +4,27 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { HttpStatus } from 'http-status-ts';
 import { TStudent } from './student.interface';
+import QueryBuilder from '../../builder/QueryBuilder';
 
 const getAllStudentsFromDb = async (query: Record<string, unknown>) => {
-  const result = await Student.find()
-    .populate('admissionSemester')
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    });
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await studentQuery.modelQuery;
   return result;
 };
 const getSingleStudentsFromDb = (id: string) => {
