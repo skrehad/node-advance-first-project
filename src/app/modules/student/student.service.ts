f1ae49a79d6683ab6534d6bae4dbe1672b340ff4
rehadhasan <rehadhasan664@gmail.com>
2024-12-08 01:52:52 +0600

    update student services

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -84,8 +84,6 @@ const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
     }
   }
 
-  // console.log(modifiedUpdatedData);
-
   const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
     new: true,
     runValidators: true,
