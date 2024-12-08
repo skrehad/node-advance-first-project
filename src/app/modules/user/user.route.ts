5e279bc49ba1237caa606cdb3a9c82235a350c82
rehadhasan <rehadhasan664@gmail.com>
2024-12-08 23:53:35 +0600

    user controller update

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { UserControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { studentValidations } from '../student/student.validation';
+import { createFacultyValidationSchema } from '../faculty/faculty.validation';
+import { createAdminValidationSchema } from '../admin/admin.validation';
 
 const router = express.Router();
 
@@ -10,6 +12,18 @@ router.post(
   '/create-student',
   validateRequest(studentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
+);
+
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty,
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserControllers.createAdmin,
 );
 
 export const UserRoutes = router;
