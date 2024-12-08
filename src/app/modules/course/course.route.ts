8e6f7b9a9a45351bd51646db562dc7c17e97a0a1
rehadhasan <rehadhasan664@gmail.com>
2024-12-09 01:59:35 +0600

    added comment in admin controller

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -15,11 +15,11 @@ router.get('/', CourseControllers.getAllCourses);
 
 router.get('/:id', CourseControllers.getSingleCourse);
 
-// router.patch(
-//   '/:id',
-//   validateRequest(CourseValidations.updateCourseValidationSchema),
-//   CourseControllers.updateCourse,
-// );
+router.patch(
+  '/:id',
+  validateRequest(CourseValidations.updateCourseValidationSchema),
+  CourseControllers.updateCourse,
+);
 
 router.delete('/:id', CourseControllers.deleteCourse);
 
