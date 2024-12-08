d93b3bf14a3249150a45bd7b833aab28b094dabd
rehadhasan <rehadhasan664@gmail.com>
2024-12-09 01:10:54 +0600

    comment course faculty schema

--- src/app/modules/course/course.model.ts
+++ src/app/modules/course/course.model.ts
@@ -57,21 +57,21 @@ const courseSchema = new Schema<TCourse>(
 
 export const Course = model<TCourse>('Course', courseSchema);
 
-const courseFacultySchema = new Schema<TCourseFaculty>({
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: 'Course',
-    unique: true,
-  },
-  faculties: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Faculty',
-    },
-  ],
-});
+// const courseFacultySchema = new Schema<TCourseFaculty>({
+//   course: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'Course',
+//     unique: true,
+//   },
+//   faculties: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Faculty',
+//     },
+//   ],
+// });
 
-export const CourseFaculty = model<TCourseFaculty>(
-  'CourseFaculty',
-  courseFacultySchema,
-);
+// export const CourseFaculty = model<TCourseFaculty>(
+//   'CourseFaculty',
+//   courseFacultySchema,
+// );
