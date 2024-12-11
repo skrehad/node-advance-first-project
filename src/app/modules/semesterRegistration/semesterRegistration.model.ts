f609f0a8ac8a1302cbe54b7aaedb0aeb202ce83f
rehadhasan <rehadhasan664@gmail.com>
2024-12-11 23:26:18 +0600

    service working

--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,10 +1,43 @@
-import { Types } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
+import { TSemesterRegistration } from './semesterRegistration.interface';
 
-export type TSemesterRegistration = {
-  academicSemester: Types.ObjectId;
-  status: 'UPCOMING' | 'ONGOING' | 'ENDED';
-  startDate: Date;
-  endDate: Date;
-  minCredit: number;
-  maxCredit: number;
-};
+const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegistration>(
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: 'AcademicSemester',
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const SemesterRegistration = mongoose.model<TSemesterRegistration>(
+  'SemesterRegistration',
+  semesterRegistrationSchema,
+);
