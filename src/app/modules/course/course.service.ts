bd85fd16e4fa79bacc699a4c7592c0f49c49d3dd
rehadhasan <rehadhasan664@gmail.com>
2024-12-11 16:07:56 +0600

    update course services

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,10 +1,9 @@
-// import httpStatus from 'http-status';
 import mongoose from 'mongoose';
 import QueryBuilder from '../../builder/QueryBuilder';
 import AppError from '../../errors/AppError';
 import { CourseSearchableFields } from './course.constant';
-import { TCourse } from './course.interface';
-import { Course } from './course.model';
+import { TCourse, TCourseFaculty } from './course.interface';
+import { Course, CourseFaculty } from './course.model';
 import { HttpStatus } from 'http-status-ts';
 
 const createCourseIntoDB = async (payload: TCourse) => {
@@ -148,21 +147,21 @@ const assignFacultiesWithCourseIntoDB = async (
   return result;
 };
 
-// const removeFacultiesFromCourseFromDB = async (
-//   id: string,
-//   payload: Partial<TCourseFaculty>,
-// ) => {
-//   const result = await CourseFaculty.findByIdAndUpdate(
-//     id,
-//     {
-//       $pull: { faculties: { $in: payload } },
-//     },
-//     {
-//       new: true,
-//     },
-//   );
-//   return result;
-// };
+const removeFacultiesFromCourseFromDB = async (
+  id: string,
+  payload: Partial<TCourseFaculty>,
+) => {
+  const result = await CourseFaculty.findByIdAndUpdate(
+    id,
+    {
+      $pull: { faculties: { $in: payload } },
+    },
+    {
+      new: true,
+    },
+  );
+  return result;
+};
 
 export const CourseServices = {
   createCourseIntoDB,
@@ -170,6 +169,6 @@ export const CourseServices = {
   getSingleCourseFromDB,
   updateCourseIntoDB,
   deleteCourseFromDB,
-  // assignFacultiesWithCourseIntoDB,
-  // removeFacultiesFromCourseFromDB,
+  assignFacultiesWithCourseIntoDB,
+  removeFacultiesFromCourseFromDB,
 };
