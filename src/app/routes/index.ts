77ea93488637e547600629aa7e8951ad209ed981
rehadhasan <rehadhasan664@gmail.com>
2024-12-11 23:28:19 +0600

    adding semester-registrations route in the main route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,7 @@ import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academic
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
 import { AdminRoutes } from '../modules/admin/admin.route';
 import { CourseRoutes } from '../modules/course/course.route';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
 
 const router = Router();
 
