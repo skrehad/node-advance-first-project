c7d7d7f54d1b3193ebbd7e9b0f5719c179010787
rehadhasan <rehadhasan664@gmail.com>
2024-12-11 16:02:40 +0600

    assignFacultiesWithCourse route controller creation

--- src/app/modules/course/course.controller.ts
+++ src/app/modules/course/course.controller.ts
@@ -61,22 +61,22 @@ const deleteCourse = catchAsync(async (req, res) => {
   });
 });
 
-// const assignFacultiesWithCourse = catchAsync(async (req, res) => {
-//   const { courseId } = req.params;
-//   const { faculties } = req.body;
+const assignFacultiesWithCourse = catchAsync(async (req, res) => {
+  const { courseId } = req.params;
+  const { faculties } = req.body;
 
-//   const result = await CourseServices.assignFacultiesWithCourseIntoDB(
-//     courseId,
-//     faculties,
-//   );
+  const result = await CourseServices.assignFacultiesWithCourseIntoDB(
+    courseId,
+    faculties,
+  );
 
-//   sendResponse(res, {
-//     statusCode: HttpStatus.OK,
-//     success: true,
-//     message: 'Faculties assigned  successfully',
-//     data: result,
-//   });
-// });
+  sendResponse(res, {
+    statusCode: HttpStatus.OK,
+    success: true,
+    message: 'Faculties assigned  successfully',
+    data: result,
+  });
+});
 
 // const removeFacultiesFromCourse = catchAsync(async (req, res) => {
 //   const { courseId } = req.params;
@@ -101,6 +101,6 @@ export const CourseControllers = {
   getAllCourses,
   updateCourse,
   deleteCourse,
-  //   assignFacultiesWithCourse,
+  assignFacultiesWithCourse,
   //   removeFacultiesFromCourse,
 };
