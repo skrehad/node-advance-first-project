024f805fb0a3412239fd71db5ef556f6f3b85d63
rehadhasan <rehadhasan664@gmail.com>
2024-12-14 18:48:33 +0600

    update offered model

--- src/app/modules/offeredCourse/offeredCourse.model.ts
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import { TOfferedCourse } from './offeredCourse.interface';
+import { Days } from './offeredCourse.constant';
 
 const offeredCourseSchema = new mongoose.Schema<TOfferedCourse>(
   {
