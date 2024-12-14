1b6057a7c6ba809169db00bbc4bacf441880e7b1
rehadhasan <rehadhasan664@gmail.com>
2024-12-14 19:26:56 +0600

    adding offered course route in the main route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,6 +8,7 @@ import { FacultyRoutes } from '../modules/faculty/faculty.route';
 import { AdminRoutes } from '../modules/admin/admin.route';
 import { CourseRoutes } from '../modules/course/course.route';
 import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.route';
 
 const router = Router();
 
@@ -48,10 +49,10 @@ const moduleRoutes = [
     path: '/semester-registrations',
     route: semesterRegistrationRoutes,
   },
-  // {
-  //   path: '/offered-courses',
-  //   route: offeredCourseRoutes,
-  // },
+  {
+    path: '/offered-courses',
+    route: offeredCourseRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
