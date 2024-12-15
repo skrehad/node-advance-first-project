3e74699e21b249395d2f41b4ceaafdb890f528af
rehadhasan <rehadhasan664@gmail.com>
2024-12-15 23:46:12 +0600

    added jwt in faculty

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { FacultyControllers } from './faculty.controller';
 import { updateFacultyValidationSchema } from './faculty.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
@@ -15,6 +17,10 @@ router.patch(
 
 router.delete('/:id', FacultyControllers.deleteFaculty);
 
-router.get('/', FacultyControllers.getAllFaculties);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.faculty),
+  FacultyControllers.getAllFaculties,
+);
 
 export const FacultyRoutes = router;
