3762bf202b87f0747181b53cac002e8fa61f0560
rehadhasan <rehadhasan664@gmail.com>
2024-12-16 00:35:08 +0600

    user route update

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,12 +4,15 @@ import validateRequest from '../../middlewares/validateRequest';
 import { studentValidations } from '../student/student.validation';
 import { createFacultyValidationSchema } from '../faculty/faculty.validation';
 import { createAdminValidationSchema } from '../admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
 // for create student
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(studentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
