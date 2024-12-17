6f5df128c7d99ee9c9904c2ec4c6701532f927aa
rehadhasan <rehadhasan664@gmail.com>
2024-12-18 03:07:22 +0600

    update academic semester

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,23 +2,32 @@ import express from 'express';
 import { AcademicSemesterControllers } from './academicSemester.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterValidations } from './academicSemester.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  auth(USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidations.createAcademicSemesterValidationSchema,
   ),
   AcademicSemesterControllers.createAcademicSemester,
 );
-router.get('/', AcademicSemesterControllers.getAllAcademicSemester);
+router.get(
+  '/',
+  auth(USER_ROLE.admin),
+  AcademicSemesterControllers.getAllAcademicSemester,
+);
 router.get(
   '/:semesterId',
+  auth(USER_ROLE.admin),
   AcademicSemesterControllers.getSingleAcademicSemester,
 );
 router.patch(
   '/:semesterId',
+  auth(USER_ROLE.admin),
   validateRequest(
     AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
   ),
