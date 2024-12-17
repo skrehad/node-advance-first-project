f8073abf11a9ba45490f0ff169dd7a76c1e5cc9f
rehadhasan <rehadhasan664@gmail.com>
2024-12-18 03:13:18 +0600

    semester registration

--- src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,26 +2,35 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { SemesterRegistrationController } from './semesterRegistration.controller';
 import { SemesterRegistrationValidations } from './semesterRegistration.validation';
+import { USER_ROLE } from '../user/user.constant';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post(
   '/create-semester-registration',
+  auth(USER_ROLE.admin),
   validateRequest(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
   SemesterRegistrationController.createSemesterRegistration,
 );
 
-router.get('/', SemesterRegistrationController.getAllSemesterRegistrations);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.student),
+  SemesterRegistrationController.getAllSemesterRegistrations,
+);
 
 router.get(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.student),
   SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin),
   validateRequest(
     SemesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
   ),
@@ -30,6 +39,7 @@ router.patch(
 
 router.delete(
   '/:id',
+  auth(USER_ROLE.admin),
   SemesterRegistrationController.deleteSemesterRegistration,
 );
 
