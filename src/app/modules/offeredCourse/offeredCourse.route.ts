cee056dc0b4542b69946de76fab17179169bf83c
rehadhasan <rehadhasan664@gmail.com>
2024-12-18 03:15:19 +0600

    offer courses route update

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -2,25 +2,30 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseControllers } from './offeredCourse.controller';
 import { OfferedCourseValidations } from './offeredCourse.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
+
+router.post(
+  '/create-offered-course',
+  auth(USER_ROLE.admin),
+  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
+  OfferedCourseControllers.createOfferedCourse,
+);
 
 router.get('/', OfferedCourseControllers.getAllOfferedCourses);
 
 router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
 
-router.post(
-  '/create-offered-course',
-  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
-  OfferedCourseControllers.createOfferedCourse,
-);
-
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin),
   validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
   OfferedCourseControllers.updateOfferedCourse,
 );
 
-router.delete('/:id', OfferedCourseControllers.deleteOfferedCourse);
+auth(USER_ROLE.admin),
+  router.delete('/:id', OfferedCourseControllers.deleteOfferedCourse);
 
 export const offeredCourseRoutes = router;
