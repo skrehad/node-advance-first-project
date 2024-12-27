e849bcdc05fa725bbe491879328ed1d2d8bf42e4
rehadhasan <rehadhasan664@gmail.com>
2024-12-28 01:16:14 +0600

    update superadmin email

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -4,7 +4,7 @@ import { User } from '../modules/user/user.model';
 
 const superUser = {
   id: '0001',
-  email: 'abedinforhan@gmail.com',
+  email: 'rehadhasan@gmail.com',
   password: config.super_admin_password,
   needsPasswordChange: false,
   role: USER_ROLE.superAdmin,
