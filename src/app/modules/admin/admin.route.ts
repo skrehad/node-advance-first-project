13ef19add2d42f266018424d5e002b74ac068b9a
rehadhasan <rehadhasan664@gmail.com>
2024-12-28 02:08:07 +0600

    added super admin role in admin route

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -7,17 +7,29 @@ import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', auth(USER_ROLE.admin), AdminControllers.getAllAdmins);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  AdminControllers.getAllAdmins,
+);
 
-router.get('/:id', auth(USER_ROLE.admin), AdminControllers.getSingleAdmin);
+router.get(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  AdminControllers.getSingleAdmin,
+);
 
 router.patch(
   '/:id',
-  auth(USER_ROLE.admin),
+  auth(USER_ROLE.superAdmin),
   validateRequest(updateAdminValidationSchema),
   AdminControllers.updateAdmin,
 );
 
-router.delete('/:adminId', auth(USER_ROLE.admin), AdminControllers.deleteAdmin);
+router.delete(
+  '/:adminId',
+  auth(USER_ROLE.superAdmin),
+  AdminControllers.deleteAdmin,
+);
 
 export const AdminRoutes = router;
