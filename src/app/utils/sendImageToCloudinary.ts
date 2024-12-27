f5eff6cc136cf587ccb150014d4ea0724565febf
rehadhasan <rehadhasan664@gmail.com>
2024-12-28 04:00:43 +0600

    update send image cloudinary file

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -9,7 +9,10 @@ cloudinary.config({
   api_secret: config.cloudinary_api_secret,
 });
 
-export const sendImageToCloudinary = (imageName: string, path: string) => {
+export const sendImageToCloudinary = (
+  imageName: string,
+  path: string,
+): Promise<Record<string, unknown>> => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
       path,
@@ -17,8 +20,11 @@ export const sendImageToCloudinary = (imageName: string, path: string) => {
       function (error, result) {
         if (error) {
           reject(error);
+        } else if (result) {
+          resolve(result); // Ensure result is defined
+        } else {
+          reject(new Error('Upload failed with an undefined result.'));
         }
-        resolve(result);
         // delete a file asynchronously
         fs.unlink(path, (err) => {
           if (err) {
